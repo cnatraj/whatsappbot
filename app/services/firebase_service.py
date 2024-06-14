064882336a59b77d72d1ff77d76ff13de5e01c67
Cartic Natraj <cnatraj@gmail.com>
2024-06-13 23:31:46 -0700

    handling newlines in private key

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -14,7 +14,7 @@ cred_json = {
   "type": os.getenv("FB_TYPE"),
   "project_id": os.getenv("FB_PROJECT_ID"),
   "private_key_id": os.getenv("FB_PRIVATE_KEY_ID"),
-  "private_key": os.getenv("FB_PRIVATE_KEY"),
+  "private_key": os.getenv("FB_PRIVATE_KEY").replace('\\n', '\n'),
   "client_email": os.getenv("FB_CLIENT_EMAIL"),
   "client_id": os.getenv("FB_CLIENT_ID"),
   "auth_uri": os.getenv("FB_AUTH_URI"),
